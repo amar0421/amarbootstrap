4fe3899a381593b97f5d0c678eda31c7cc60e581
amar0421 <amarthakare91@gmail.com>
2015-11-03 06:06:58 +0530

    MDT GUI Changes Finle

---
+++ login/js/start.js
@@ -0,0 +1,10 @@
+	
+
+/* 	https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
+	https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js
+	https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css
+	https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css
+	
+	$(document).ready(function(){
+		$(".lang").modal('show');
+	}); */
